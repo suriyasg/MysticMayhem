6770610de02e8c7218f4e78821ff68d0376128b3
unknown <prathhp231@gmail.com>
2024-03-03 19:02:36 +0530

    Modified the user class added buy character method to apply in BuyCharacter removed previously created static objects

--- BuyCharacter.java
+++ BuyCharacter.java
@@ -6,8 +6,8 @@ import java.io.ObjectOutputStream;
  * BuyCharacter
  */
 public class BuyCharacter {
-    public static void render(User currentUser) throws ClassNotFoundException, IOException{
-        System.out.println(currentUser.getName()+", Welcome To Our Shop! spend your monye wisely!"); // funny pharses
+    public static void render(User currentUser) throws ClassNotFoundException, IOException {
+        System.out.println(currentUser.getName() + ", Welcome To Our Shop! spend your monye wisely!"); // funny pharses
         System.out.println("Which Charecter you want to buy?");
         System.out.println("1. Archer");
         System.out.println("2. Knight");
@@ -17,7 +17,7 @@ public class BuyCharacter {
         System.out.println("press 6 to go to main menu");
         System.out.println("Enter suitable number");
 
-        int choice =  InputProcessor.getInt();
+        int choice = InputProcessor.getInt();
         if (choice == 1) {
             System.out.println("Which Archer you want to buy?");
             System.out.println("1. Shooter from hillcr");
@@ -28,71 +28,36 @@ public class BuyCharacter {
             System.out.println("press 6 to go back");
             System.out.println("Enter suitable number");
 
-            choice =  InputProcessor.getInt();
-            double remainingCoins;
-            if (choice == 1) {    
+            choice = InputProcessor.getInt();
+
+            if (choice == 1) {
                 Archer newShooter = Archer.makeShooter();
-                remainingCoins = currentUser.getCoins()-newShooter.getPrice();
-                if(remainingCoins>=0){
-                    currentUser.characters.add(newShooter);
-                    currentUser.setCoins(remainingCoins);
-                }else{
-                    System.out.println("You are poor! you don't have enough money to buy lol");
-                }
-                System.out.println("Your current balance :" + currentUser.getCoins());
-            }else if (choice == 2) {
+                currentUser.buyCharacter(newShooter);
+            } else if (choice == 2) {
                 Archer newRanger = Archer.makeRanger();
-                remainingCoins = currentUser.getCoins()-newRanger.getPrice();
-                if(remainingCoins>=0){
-                    currentUser.characters.add(newRanger);
-                    currentUser.setCoins(remainingCoins);
-                }else{
-                    System.out.println("You are poor! you don't have enough money to buy lol");
-                }
-                System.out.println("Your current balance :" + currentUser.getCoins());
-            }else if (choice == 3) {
-                Archer newSunFire = Archer.makeSunFire();
-                remainingCoins = currentUser.getCoins()-newSunFire.getPrice();
-                if(remainingCoins>=0){
-                    currentUser.characters.add(newSunFire);
-                    currentUser.setCoins(remainingCoins);
-                }else{
-                    System.out.println("You are poor! you don't have enough money to buy lol");
-                }
-                System.out.println("Your current balance :" + currentUser.getCoins());
-            }else if (choice == 4) {
+                currentUser.buyCharacter(newRanger);
+            } else if (choice == 3) {
+                Archer newSunfire = Archer.makeSunFire();
+                currentUser.buyCharacter(newSunfire);
+            } else if (choice == 4) {
                 Archer newZing = Archer.makeZing();
-                remainingCoins = currentUser.getCoins()-newZing.getPrice();
-                if(remainingCoins>=0){
-                    currentUser.characters.add(newZing);
-                    currentUser.setCoins(remainingCoins);
-                }else{
-                    System.out.println("You are poor! you don't have enough money to buy lol");
-                }
-                System.out.println("Your current balance :" + currentUser.getCoins());
-            }else if (choice == 5) {
-                Archer newSaggitarius = Archer.makeSaggitarius();
-                remainingCoins = currentUser.getCoins()-newSaggitarius.getPrice();
-                if(remainingCoins>=0){
-                    currentUser.characters.add(newSaggitarius);
-                    currentUser.setCoins(remainingCoins);
-                }else{
-                    System.out.println("You are poor! you don't have enough money to buy lol");
-                }
-                System.out.println("Your current balance :" + currentUser.getCoins());
-            }else{
+                currentUser.buyCharacter(newZing);
+            } else if (choice == 5) {
+                Archer saggitarius = Archer.makeSaggitarius();
+                currentUser.buyCharacter(saggitarius);
+            } else {
                 BuyCharacter.render(currentUser);
             }
 
-        }else if (choice == 2) {
-            
-        }else if (choice == 3) {
-            
-        }else if (choice == 4) {
-            
-        }else if (choice == 5) {
-            
-        }else{
+        } else if (choice == 2) {
+
+        } else if (choice == 3) {
+
+        } else if (choice == 4) {
+
+        } else if (choice == 5) {
+
+        } else {
             MainMenu.render(currentUser.getUserName());
         }
 
@@ -102,6 +67,5 @@ public class BuyCharacter {
         currentUserOut.close();
         currentUserFile.close();
     }
-
 
 }
