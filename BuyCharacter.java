904e619166bf11e1e76bdd0410c4d4e5a256cec4
unknown <prathhp231@gmail.com>
2024-03-03 16:45:54 +0530

    Bugs solved Error Handling added when getting input

--- BuyCharacter.java
+++ BuyCharacter.java
@@ -6,8 +6,10 @@ import java.io.ObjectOutputStream;
  * BuyCharacter
  */
 public class BuyCharacter {
-    public static void render(User currentUser) throws ClassNotFoundException, IOException{
-        System.out.println(currentUser.getName()+", Welcome To Our Shop! spend your monye wisely!"); // funny pharses
+    public static void render(User currentUser) throws ClassNotFoundException, IOException {
+        System.out.println("Hello" + currentUser.getName()
+                + ", Welcome To Our Shop!\nYou should buy one Character from each Category, spend your money wisely!"); // funny
+                                                                                                                        // pharses
         System.out.println("Which Charecter you want to buy?");
         System.out.println("1. Archer");
         System.out.println("2. Knight");
@@ -17,7 +19,7 @@ public class BuyCharacter {
         System.out.println("press 6 to go to main menu");
         System.out.println("Enter suitable number");
 
-        int choice =  InputProcessor.getInt();
+        int choice = InputProcessor.getInt();
         if (choice == 1) {
             System.out.println("Which Archer you want to buy?");
             System.out.println("1. Shooter from hillcr");
@@ -28,71 +30,71 @@ public class BuyCharacter {
             System.out.println("press 6 to go back");
             System.out.println("Enter suitable number");
 
-            choice =  InputProcessor.getInt();
+            choice = InputProcessor.getInt();
             double remainingCoins;
-            if (choice == 1) {    
+            if (choice == 1) {
                 Archer newShooter = Archer.makeShooter();
-                remainingCoins = currentUser.getCoins()-newShooter.getPrice();
-                if(remainingCoins>=0){
+                remainingCoins = currentUser.getCoins() - newShooter.getPrice();
+                if (remainingCoins >= 0) {
                     currentUser.characters.add(newShooter);
                     currentUser.setCoins(remainingCoins);
-                }else{
+                } else {
                     System.out.println("You are poor! you don't have enough money to buy lol");
                 }
                 System.out.println("Your current balance :" + currentUser.getCoins());
-            }else if (choice == 2) {
+            } else if (choice == 2) {
                 Archer newRanger = Archer.makeRanger();
-                remainingCoins = currentUser.getCoins()-newRanger.getPrice();
-                if(remainingCoins>=0){
+                remainingCoins = currentUser.getCoins() - newRanger.getPrice();
+                if (remainingCoins >= 0) {
                     currentUser.characters.add(newRanger);
                     currentUser.setCoins(remainingCoins);
-                }else{
+                } else {
                     System.out.println("You are poor! you don't have enough money to buy lol");
                 }
                 System.out.println("Your current balance :" + currentUser.getCoins());
-            }else if (choice == 3) {
+            } else if (choice == 3) {
                 Archer newSunFire = Archer.makeSunFire();
-                remainingCoins = currentUser.getCoins()-newSunFire.getPrice();
-                if(remainingCoins>=0){
+                remainingCoins = currentUser.getCoins() - newSunFire.getPrice();
+                if (remainingCoins >= 0) {
                     currentUser.characters.add(newSunFire);
                     currentUser.setCoins(remainingCoins);
-                }else{
+                } else {
                     System.out.println("You are poor! you don't have enough money to buy lol");
                 }
                 System.out.println("Your current balance :" + currentUser.getCoins());
-            }else if (choice == 4) {
+            } else if (choice == 4) {
                 Archer newZing = Archer.makeZing();
-                remainingCoins = currentUser.getCoins()-newZing.getPrice();
-                if(remainingCoins>=0){
+                remainingCoins = currentUser.getCoins() - newZing.getPrice();
+                if (remainingCoins >= 0) {
                     currentUser.characters.add(newZing);
                     currentUser.setCoins(remainingCoins);
-                }else{
+                } else {
                     System.out.println("You are poor! you don't have enough money to buy lol");
                 }
                 System.out.println("Your current balance :" + currentUser.getCoins());
-            }else if (choice == 5) {
+            } else if (choice == 5) {
                 Archer newSaggitarius = Archer.makeSaggitarius();
-                remainingCoins = currentUser.getCoins()-newSaggitarius.getPrice();
-                if(remainingCoins>=0){
+                remainingCoins = currentUser.getCoins() - newSaggitarius.getPrice();
+                if (remainingCoins >= 0) {
                     currentUser.characters.add(newSaggitarius);
                     currentUser.setCoins(remainingCoins);
-                }else{
+                } else {
                     System.out.println("You are poor! you don't have enough money to buy lol");
                 }
                 System.out.println("Your current balance :" + currentUser.getCoins());
-            }else{
+            } else {
                 BuyCharacter.render(currentUser);
             }
 
-        }else if (choice == 2) {
-            
-        }else if (choice == 3) {
-            
-        }else if (choice == 4) {
-            
-        }else if (choice == 5) {
-            
-        }else{
+        } else if (choice == 2) {
+
+        } else if (choice == 3) {
+
+        } else if (choice == 4) {
+
+        } else if (choice == 5) {
+
+        } else {
             MainMenu.render(currentUser.getUserName());
         }
 
@@ -102,6 +104,5 @@ public class BuyCharacter {
         currentUserOut.close();
         currentUserFile.close();
     }
-
 
 }
--- InputProcessor.java
+++ InputProcessor.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class InputProcessor {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static int getInt(){
-        String nexString = scanner.nextLine(); // what if user enter non numerical input
-        return Integer.parseInt(nexString);
+    public static int getInt() {
+        try {
+            int userInput = Integer.parseInt(scanner.nextLine());
+            return userInput;
+        } catch (NumberFormatException e) {
+            System.out.print("Please Enter a Number Value: ");
+            return getInt();
+        }
+
+        // String nexString = scanner.nextLine(); // what if user enter non numerical
+        // input
+        // return Integer.parseInt(nexString);
     }
 
-    public static String getString(){
-        String nexString = scanner.nextLine(); 
+    public static String getString() {
+        String nexString = scanner.nextLine();
         return nexString;
     }
 
