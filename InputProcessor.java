6a2c91051513a6721f8e988e352ca8454ce83421
unknown <prathhp231@gmail.com>
2024-03-10 07:25:44 +0530

    Input String validation

--- InputProcessor.java
+++ InputProcessor.java
@@ -2,26 +2,32 @@ import java.util.Scanner;
 
 public class InputProcessor {
     private static Scanner scanner = new Scanner(System.in);
-    // handle when people give out of bound numbers via InputProcessor by giving parameters
+
+    // handle when people give out of bound numbers via InputProcessor by giving
+    // parameters
     public static int getInt(int low, int high) {
         try {
-            int choice =  Integer.parseInt(scanner.nextLine());
-            if(choice >= low && choice <= high){
+            int choice = Integer.parseInt(scanner.nextLine());
+            if (choice >= low && choice <= high) {
                 return choice;
-            }else{
+            } else {
                 System.out.print("Please Enter a Number within range: ");
-                return getInt(low,high);
+                return getInt(low, high);
             }
         } catch (NumberFormatException e) {
             System.out.print("Please Enter a Number: ");
-            return getInt(low,high);
+            return getInt(low, high);
         }
 
     }
 
     public static String getString() {
-        String nexString = scanner.nextLine();
-        return nexString;
+        String nextString = scanner.nextLine();
+        if (nextString.equals("") && nextString.trim().isEmpty()) {
+            System.out.print("Please Enter a valid name:");
+            return getString();
+        }
+        return nextString;
     }
 
 }
